43ab6e08bf72ecf71993a74a565758c1fe16d878
@hexlet/cli <support@hexlet.io>
2023-12-03 20:22:08 +0300

    submit java-oop-ru/code-generation

--- java-oop-ru/code-generation/src/main/java/exercise/Car.java
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -28,31 +28,4 @@ class Car {
         return car;
     }
     // END
-
-    public static void main(String[] args) {
-        User owner = new User(1, "Ivan", "P", 25);
-// Вызываем автоматически сгенерированный геттер
-        System.out.println(owner.getFirstName());
-        Car car = new Car(1, "bmv", "x5", "black", owner);
-        String jsonRepresentation = car.serialize();
-        System.out.println(jsonRepresentation); // =>
-/*
-{
-  "id":1,
-  "brand":"bmv",
-  "model":"x5",
-  "color":"black",
-  "owner":{
-      "id":1,
-      "firstName":"Ivan",
-      "lastName":"P",
-      "age":25
-  }
-}
-*/
-
-        Car instance = Car.unserialize(jsonRepresentation);
-        System.out.println(instance.getBrand()); // "bmv"
-        System.out.println(instance.getModel()); // "x5"
-    }
 }
