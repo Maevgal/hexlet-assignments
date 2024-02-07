b1605aedd6b65c59e2b39529556df2b3e7c93858
@hexlet/cli <support@hexlet.io>
2024-02-07 15:06:21 +0700

    submit java-web-ru/flash

---
+++ java-web-ru/flash/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,60 @@
+package exercise.controller;
+
+import exercise.dto.posts.BuildPostPage;
+import exercise.dto.posts.PostPage;
+import exercise.dto.posts.PostsPage;
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.util.NamedRoutes;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.validation.ValidationException;
+
+import java.util.Collections;
+
+public class PostsController {
+
+    public static void build(Context ctx) {
+        var page = new BuildPostPage();
+        ctx.render("posts/build.jte", Collections.singletonMap("page", page));
+    }
+
+    // BEGIN
+    public static void create(Context ctx) {
+
+        var bodyPost = ctx.formParam("body");
+        try {
+            var namePost = ctx.formParamAsClass("name", String.class)
+                    .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
+                    .get();
+            Post post = new Post(namePost, bodyPost);
+            PostRepository.save(post);
+            ctx.sessionAttribute("flash", "Пост был успешно создан!");
+            ctx.sessionAttribute("flash-type", "success");
+            ctx.redirect(NamedRoutes.postsPath());
+        } catch (ValidationException e) {
+            var namePost = ctx.formParam("name");
+            var post = new BuildPostPage(namePost, bodyPost, e.getErrors());
+            ctx.render("posts/build.jte", Collections.singletonMap("post", post)).status(422);
+        }
+
+    }
+
+    public static void index(Context ctx) {
+        var posts = PostRepository.getEntities();
+        var page = new PostsPage(posts);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+        ctx.render("posts/index.jte", Collections.singletonMap("page", page));
+    }
+    // END
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var post = PostRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Post not found"));
+
+        var page = new PostPage(post);
+        ctx.render("posts/show.jte", Collections.singletonMap("page", page));
+    }
+}
