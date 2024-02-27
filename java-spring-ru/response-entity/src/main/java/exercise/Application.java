184ca84bb93313fa5901a4ecf22e5798beccdde8
@hexlet/cli <support@hexlet.io>
2024-02-27 17:00:48 +0700

    submit java-spring-ru/response-entity

---
+++ java-spring-ru/response-entity/src/main/java/exercise/Application.java
@@ -0,0 +1,76 @@
+package exercise;
+
+import exercise.model.Post;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @GetMapping("/posts") // список всех постов
+    public ResponseEntity<List<Post>> index(@RequestParam(defaultValue = "10") Integer limit) {
+        var result = posts.stream().limit(limit).toList();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(posts.size()))
+                .body(result);
+    }
+
+    @GetMapping("/posts/{id}") // просмотр конкретного поста
+    public ResponseEntity<Post> show(@PathVariable String id) {
+        var post = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        return ResponseEntity.of(post);
+    }
+
+    @PostMapping("/posts") // создание нового поста
+    public ResponseEntity<Post> create(@RequestBody Post post) {
+        posts.add(post);
+        return ResponseEntity.status(201)
+                .body(post);
+    }
+
+    @PutMapping("/posts/{id}") // обновление поста
+    public ResponseEntity<Post> update(@PathVariable String id, @RequestBody Post data) {
+        var maybePage = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (maybePage.isPresent()) {
+            var page = maybePage.get();
+            page.setId(data.getId());
+            page.setTitle(data.getTitle());
+            page.setBody(data.getBody());
+            return ResponseEntity.ok()
+                    .body(page);
+        }
+
+        return ResponseEntity.status(204)
+                .body(data);
+    }
+    // END
+
+    @DeleteMapping("/posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+}
